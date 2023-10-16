5c40ec26f8a737e95a48b9a7886b9f6ddbb5d41a
mortolait <contatomortola@outlook.com>
2023-10-16 19:08:39 -0300

    feat: alteracao no formato de dado exibido no catalogo

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    // Adicione esta configuração para copiar os arquivos estáticos para a pasta de build
+    assetsDir: 'assets',
+  }
 })
